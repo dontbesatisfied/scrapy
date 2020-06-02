5688ab9b2d841c5a0764fd36734005699fd1001a
SHwan93 <zxcvzxcv93@naver.com>
2020-06-02 19:11:29 +0900

    login

---
+++ quotes_login_spider/quotes_login_spider/spiders/login.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider
+from scrapy.http import FormRequest
+
+
+class LoginSpider(Spider):
+    name = 'login'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/login']
+
+    def parse(self, response):
+        csrf_token = response.xpath(
+            '//*[@name="csrf_token"]/@value').extract_first()
+        yield FormRequest('http://quotes.toscrape.com/login', formdata={
+            'csrf_token': csrf_token,
+            'username': 'scrapy',
+            'password': 'pw'
+        }, callback=self.parse_after_login)
+
+    def parse_after_login(self, response):
+        if response.xpath('//a[text()="Logout"]'):
+            self.log('You logged in')
